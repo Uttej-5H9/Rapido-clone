e65dd207f317e6660d2f91cadaa8a41fc81b8dd6
Uttej <uttejvaddi002@gmail.com>
2026-05-07 13:09:43 +0530

    Initial commit - microservices backend setup

---
+++ rapido-clone-backend/src/main/java/rapido_clone_backend/RapidoCloneBackendApplication.java
@@ -0,0 +1,13 @@
+package rapido_clone_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidoCloneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapidoCloneBackendApplication.class, args);
+	}
+
+}
---
+++ rapido-clone-backend/src/test/java/rapido_clone_backend/RapidoCloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package rapido_clone_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapidoCloneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
